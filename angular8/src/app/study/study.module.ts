ef7f01bb56cb2794dcf16ca2f854b30adf17492a
Wilson Ferreira <wilsonferreira@example.com>
2019-08-26 13:25:57 -0300

    boostart has been added

---
+++ angular8/src/app/study/study.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CharacterComponent } from './character/character.component';
+
+
+
+@NgModule({
+  declarations: [CharacterComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class StudyModule { }
